db2d1343c49ac68a01d8dfa0d3fe2fc9719ee8ad
Jaime Escolar <jaime.escolar@gmail.com>
2016-06-30 18:28:40 +0200

    Compiler

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>Curso React Jsx Compiler</div>
+        )
+    }
+}
+
+export default App;
