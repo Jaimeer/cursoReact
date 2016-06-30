9c5ba5dbb600a5296471f07d3de8ed5a23e25076
Jaime Escolar <jaime.escolar@gmail.com>
2016-06-30 18:57:53 +0200

    lifecycle

--- App.js
+++ App.js
@@ -3,12 +3,19 @@ import React from 'react'
 class App extends React.Component {
 
     constructor() {
-        super();
+        super()
+        this.state = {val: 1}
+        this.update = this.update.bind(this)
+    }
+
+    update() {
+        this.setState({val: this.state.val + 1})
     }
 
     render() {
+        console.log('rendering')
         return (
-            <div>Curso React Jsx Compiler</div>
+            <button onClick={this.update}>{this.state.val}</button>
         )
     }
 }
